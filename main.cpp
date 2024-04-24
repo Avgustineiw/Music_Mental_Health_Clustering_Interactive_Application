c2f2dadab942d9942e5f8354497d3ccfb79331f0
Avgustineiw <55994897+Avgustineiw@users.noreply.github.com>
2024-04-24 11:44:23 +0300

    Add readwrite.h and respondent.h
    Added basic structure of both files, implemented some of the code. Still requires polish and more features

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "readwrite.h"
+#include "respondent.h"
+
+using namespace std;
+
+const string DATA_PATH = "music_health_data.csv";
+
+int main()
+{
+  // ReadFromCSV(DATA_PATH);
+
+  return 0;
+}
