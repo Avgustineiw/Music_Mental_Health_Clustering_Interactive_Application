ea8ab61f1bb9566e039ffc338ff80139a7a2b61d
Avgustineiw <55994897+Avgustineiw@users.noreply.github.com>
2024-05-08 13:15:07 +0300

    ClusterPoint and Cluster classes
    Both classes are necessary for all clustering algorithms

--- header.h
+++ header.h
@@ -7,23 +7,31 @@
 const string CSV_PATH = "music_health_data.csv";
 const string OUTPUT_PATH = "output.csv";
 
+
 void InitializeProgram()
 {
   vector<vector<string>> data;
   vector<Respondent> respondents;
 
-  ReadFromCSV(CSV_PATH, data);
-  DataToRespondent(data, respondents);
+  // ReadFromCSV(CSV_PATH, data);
+  // DataToRespondent(data, respondents);
 
   ofstream output_file(OUTPUT_PATH);
+  
 
-  for (size_t i = 0; i < respondents.size(); i++) {
-    // cout << respondents[i];
-    double x = 0, y = 0;
-    x = respondentToPoint(respondents[i]).GetX();
-    y = respondentToPoint(respondents[i]).GetY();
-    if (y != 0) {
-      output_file << x << "," << y << '\n';
-    }
-  }
+  ClusterPoint point = {5, 4, 0};
+  Cluster cluster = {1, point};
+
+  cluster.SetCentroidY(10);
+  std::cout << cluster.GetPoint(0).GetY();
+
+  // for (size_t i = 0; i < respondents.size(); i++) {
+  //   // cout << respondents[i];
+  //   double x = 0, y = 0;
+  //   x = respondentToPoint(respondents[i]).GetX();
+  //   y = respondentToPoint(respondents[i]).GetY();
+  //   if (y != 0) {
+  //     output_file << x << "," << y << '\n';
+  //   }
+  // }
 }
--- point.h
+++ point.h
@@ -1,3 +1,8 @@
+#include <vector>
+
+using namespace std;
+
+
 class Point
 {
 protected:
@@ -6,17 +11,102 @@ protected:
 
 public:
   Point(double x, double y): _x(x), _y(y) {};
-  double GetX() const {
+  virtual double GetX() const {
     return _x;
   }
-  double GetY() const {
+  virtual double GetY() const {
     return _y;
   }
 
-  void SetX(double x) {
+  virtual void SetX(double x) {
     this->_x = x;
   }
-  void SetY(double y) {
+  virtual void SetY(double y) {
     this->_y = y;
+  }
+};
+
+
+class ClusterPoint : public Point
+{
+private:
+  int _pointId;
+  int _clusterId;
+
+public: 
+  ClusterPoint(double x, double y, int pointId, int clusterId = 0): 
+               Point(x, y), _pointId(pointId), _clusterId(clusterId) {};
+
+  int GetPointId() const {
+    return _pointId;
+  }
+  int GetClusterId() const {
+    return _clusterId;
+  }
+
+  void SetPointId(int pointId) {
+    this->_pointId = pointId;
+  }
+  void SetClusterId(int clusterId) {
+    this->_clusterId = clusterId;
+  }
+};
+
+
+class Cluster
+{
+protected:
+  int _clusterId;
+  vector<double> _centroid;
+  vector<ClusterPoint> _points;
+
+public:
+  Cluster(int clusterId, ClusterPoint centroid): _clusterId(clusterId) {
+    this->_centroid.push_back(centroid.GetX());
+    this->_centroid.push_back(centroid.GetY());
+    this->SetPoint(centroid);
+  };
+  
+  void SetPoint(ClusterPoint point) {
+    point.SetClusterId(this->_clusterId);
+    _points.push_back(point);
+  }
+  bool RemovePoint(int pointId) {
+    int size = _points.size();
+
+    for (size_t i = 0; i < size; i++) {
+      if (_points[i].GetPointId() == pointId) {
+        _points.erase(_points.begin() + pointId);
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  void RemoveAllPoints() {
+    _points.clear();
+  }
+  Point GetPoint(int pointId) {
+    return _points[pointId];
+  }
+
+  int GetClusterSize() const {
+    return _points.size();
+  }
+  int GetClusterId() const {
+    return _clusterId;
+  }
+  double GetCentroidX() const {
+    return _centroid[0];
+  }
+  double GetCentroidY() const {
+    return _centroid[1];
+  }
+
+  void SetCentroidX(double x) {
+    this->_centroid[0] = x;
+  }
+  void SetCentroidY(double y) {
+    this->_centroid[1] = y;
   }
 };
