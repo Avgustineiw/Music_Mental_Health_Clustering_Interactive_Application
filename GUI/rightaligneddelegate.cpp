e8f9d2205326361ac1050644d2ecdfee487fd26a
Lunciare <lunci4re@yandex.ru>
2024-05-25 14:18:01 +0300

    changes

--- GUI/rightaligneddelegate.cpp
+++
@@ -1,8 +0,0 @@
-#include "rightaligneddelegate.h"
-
-void RightAlignedDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
-{
-  QStyleOptionViewItem newOption = option;
-  newOption.displayAlignment = Qt::AlignRight | Qt::AlignVCenter;
-  QStyledItemDelegate::paint(painter, newOption, index);
-}
